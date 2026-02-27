f21dcc97f7a880d4dc5dd7d3da0e96ff715df53c
oooop2914-rgb <oooop2914@gmail.com>
2026-02-27 23:41:34 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Membaca semua file di folder src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
